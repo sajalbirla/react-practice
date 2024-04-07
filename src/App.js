4f6b9cb92e766812881955c451d12e89535f9797
SajalBirla <sajalbirla71@gmail.com>
2024-04-07 20:10:58 +0530

    Update code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
       </header>
     </div>
   );
